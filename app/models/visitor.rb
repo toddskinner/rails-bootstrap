87253850e94db3be569bb53715a853476e5a176b
Todd Skinner <todd.skinner@gmail.com>
2014-07-09 21:39:02 -0600

    save data to a spreadsheet

---
+++ app/models/visitor.rb
@@ -0,0 +1,23 @@
+class Visitor < ActiveRecord::Base
+  has_no_table
+  column :favorite, :string 
+  column :comment, :string 
+  validates_presence_of :favorite
+  
+  IMAGE_LABELS = ['San Francisco', 'Sydney', 'Paris']
+  
+  def update_spreadsheet
+    connection = GoogleDrive.login(Rails.application.secrets.gmail_username, Rails.application.secrets.gmail_password)
+    ss = connection.spreadsheet_by_title('Rails-Bootstrap-Example')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Rails-Bootstrap-Example')
+    end
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    ws[last_row, 1] = Time.now
+    ws[last_row, 2] = self.favorite
+    ws[last_row, 3] = self.comment
+    ws.save
+  end
+  
+end
